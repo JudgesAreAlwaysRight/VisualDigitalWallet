a7732308ddf1cfa250961ff548eef3dfcebd6d3e
小辣稽 <1214208054@qq.com>
2021-05-01 23:21:14 +0800

    android net dev 1.4

--- VisualWallet/app/src/main/java/com/example/visualwallet/net/SplitRequest.java
+++ VisualWallet/app/src/main/java/com/example/visualwallet/net/SplitRequest.java
@@ -1,4 +1,22 @@
 package com.example.visualwallet.net;
 
-public class SplitRequest {
+public class SplitRequest extends NetRequest {
+    private static final String subUrl = "/generate";
+    private static final String reqFlag = "genSplit";
+    private String secretKey;
+    private int coeK;
+    private int coeN;
+    private String IMEI;
+    private String IMSI;
+    private String MAC;
+    private String curType;
+    private int walNo;
+
+    public SplitRequest(String secretKey, int coeK, int coeN, String curType, int walNo) {
+        this.secretKey = secretKey;
+        this.coeK = coeK;
+        this.coeN = coeN;
+        this.curType = curType;
+        this.walNo = walNo;
+    }
 }
