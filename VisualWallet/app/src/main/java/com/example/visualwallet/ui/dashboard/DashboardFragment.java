11e278de1bb6890333a61b1034b0e1afb864fd03
小辣稽 <1214208054@qq.com>
2021-05-16 16:10:29 +0800

    fix bug in main page switch

--- VisualWallet/app/src/main/java/com/example/visualwallet/ui/dashboard/DashboardFragment.java
+++ VisualWallet/app/src/main/java/com/example/visualwallet/ui/dashboard/DashboardFragment.java
@@ -4,7 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +16,47 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.visualwallet.R;
+import com.example.visualwallet.common.Constant;
+import com.example.visualwallet.net.NetCallback;
+import com.example.visualwallet.net.TransRequest;
+import com.example.visualwallet.ui.home.HomeViewModel;
+
+import java.util.Map;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
 
+    private Button transBtn;
+    private EditText editTextKey;
+    private String key = null;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
+        dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+
+        editTextKey = root.findViewById(R.id.editTextKey);
+        key = editTextKey.getText().toString();
+
+        transBtn = root.findViewById(R.id.button_trans);
+        transBtn.setOnClickListener(view_->{
+            TransRequest transRequest = new TransRequest(Constant.fromAddr, Constant.toAddr, Constant.value);
+            transRequest.setNetCallback(new NetCallback() {
+                @Override
+                public void callBack(@Nullable @org.jetbrains.annotations.Nullable Map res) {
+                }
+            });
+            if (key != null) {
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(getActivity(), "转账请求已发出，等待区块链确认", Toast.LENGTH_LONG).show();
             }
         });
+
         return root;
     }
 }
